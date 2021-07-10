18c3dd00dc065f72fda34743af80c857f3d2f55d
Shivateja Burle <shivateja.idizital@gmail.com>
2021-07-10 11:48:34 +0000

    Added Title

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Sample ToDo App using React</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           ToDo App.
